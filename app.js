399e17f5dc3a066e353ee0f8ba33422cc53358cb
chris pierre <lilchr@yahoo.com>
2018-11-27 09:46:52 -0800

    moment practice,

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("hey");
-console.log(moment())
+
+
+
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyDt5IAbDJIcr3i8f3iaenNA76EcX4TCT9k",
@@ -11,57 +13,92 @@ var config = {
 };
 firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
 
-  var trainName = "";
-  var destination = "";
-  var trainTime = "";
-  var frequency = "";
-  var clickCounter = 0;
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = "";
+var clickCounter = 0;
 
-  $(".btn").on("click", function (){
-    event.preventDefault();
+$(".btn").on("click", function () {
+  event.preventDefault();
 
-    trainName = $("#TrainName").val().trim();
-    destination = $("#destination").val().trim();
-    frequency = $("#frequency").val().trim();
-    firstTime = $("#trainTime").val().trim();
+  trainName = $("#TrainName").val().trim();
+  destination = $("#destination").val().trim();
+  frequency = $("#frequency").val().trim();
+  firstTime = $("#trainTime").val().trim();
 
 
-   clickCounter++;
+  clickCounter++;
 
-    database.ref().push({
-      "trainName": trainName,
-      "destination": destination,
-      "frequency": frequency,
-      "firstTime": firstTime,
+  database.ref().push({
+    "trainName": trainName,
+    "destination": destination,
+    "frequency": frequency,
+    "firstTime": firstTime,
 
-    });
   });
+});
 
 
-    database.ref().on("child_added", function (snapshot) {
-        
-       console.log( snapshot.val().trainName);
+database.ref().on("child_added", function (snapshot) {
 
-        var myTableRow = $("<tr>");
-        var myTable1 = $("<td>"+ snapshot.val().trainName+"</td>");
-        var myTable2 = $("<td>"+ snapshot.val().destination+"</td>");
-        var myTable3 = $("<td>"+ snapshot.val().frequency+"</td>");
-        var myTable4 = $("<td>"+ snapshot.val().trainTime+"</td>");
+  console.log(snapshot.val());
 
-        myTableRow.append(myTable1);
-        myTableRow.append(myTable2);
-        myTableRow.append(myTable3);
-        myTableRow.append(myTable4);
-
-
-$("#target").append(myTableRow);
-
-
-
-
-    });
   
+  var tFrequency = parseInt(snapshot.val().frequency);
+
+  // Time is 3:30 AM
+  var firstTime = snapshot.val().firstTime;
+  
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+  var myTableRow = $("<tr>");
+  var myTable1 = $("<td>" + snapshot.val().trainName + "</td>");
+  var myTable2 = $("<td>" + snapshot.val().destination + "</td>");
+  var myTable3 = $("<td>" + snapshot.val().frequency + "</td>");
+  var myTable4 = $("<td>" +  moment(nextTrain).format("hh:mm") + "</td>");
+  var myTable5 = $("<td>" + tMinutesTillTrain + "</td>");
+
+  myTableRow.append(myTable1);
+  myTableRow.append(myTable2);
+  myTableRow.append(myTable3);
+  myTableRow.append(myTable4);
+  myTableRow.append(myTable5);
+
+
+  $("#target").append(myTableRow);
+
+});
+
+// MOMENT
+console.log(moment());
+
+var randomDate = moment("02/14/2001", "MM/DD/YYYY");
+console.log(randomDate);
 
